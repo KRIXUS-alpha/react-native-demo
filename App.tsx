b333e93539e067c417848744ce39db925b1a4c5e
KRIXUS-alpha <kartikrockson1@gmail.com>
2022-01-25 14:30:17 +0530

    Expo init, View, ScrollView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, NativeSyntheticEvent, NativeTouchEvent, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setname] = useState<string>("Name");
+  const [people, setpeople] = useState([
+    { name: 'John', key: '1' },
+    { name: 'ansari', key: '2' },
+    { name: 'abhinav', key: '3' },
+    { name: 'niket', key: '4' },
+    { name: 'harshit', key: '5' },
+    { name: 'abdul', key: '6' },
+    { name: 'ansari', key: '7' },
+    { name: 'abhinav', key: '8' },
+    { name: 'niket', key: '9' },
+    { name: 'harshit', key: '10' },
+    { name: 'abdul', key: '11' },
+    { name: 'ansari', key: '12' },
+    { name: 'abhinav', key: '13' },
+    { name: 'niket', key: '14' },
+    { name: 'harshit', key: '15' },
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View >
+      {/* <Text style={styles.red}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{name}</Text> */}
+      {/* <Button title="Click this" onPress={()=>setname(prevState => prevState=="Name"?"changed":"Name")}/> */}
+      <ScrollView>
+        {people.map((item)=>{
+          return (
+            <View  key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+          })}
+      </ScrollView>
     </View>
   );
 }
@@ -17,4 +48,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  red: {
+    color:"red"
+  },
+  item: {
+    marginTop: 24, 
+    padding: 10, 
+    height: 30 ,
+    backgroundColor: 'pink',
+    fontSize: 10,
+    color: 'blue'
+  }
+
 });
